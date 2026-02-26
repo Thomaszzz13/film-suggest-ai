be750f9dce618b609d25ce84643e5e1a51eab556
Thomaszzz13 <thomaselnahas@gmail.com>
2026-02-26 23:24:02 +0200

    Added local film matching system

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 const button = document.getElementById("suggestBtn");
 const input = document.getElementById("userInput");
-const resultDiv = document.getElementById("results");
+const resultsDiv = document.getElementById("results");
 
-button.addEventListener("click", function() {
-    const userText = input.value;
-    
-    if (userText.trim()=== "") {
-        resultDiv.innerHTML = "<p>Please describe what you want.</p>";
+const films = [
+    { title: "Manchester by the Sea", mood: "sad" },
+    { title: "In the Mood for Love", mood: "romantic" },
+    { title: "The Grand Budapest Hotel", mood: "fun" },
+    { title: "Blade Runner 2049", mood: "dark" },
+    { title: "Eternal Sunshine of the Spotless Mind", mood: "emotional" }
+];
+
+button.addEventListener("click", function () {
+
+    const userText = input.value.toLowerCase();
+
+    if (userText.trim() === "") {
+        resultsDiv.innerHTML = "<p>Please describe what you want.</p>";
         return;
     }
 
-    resultDiv.innerHTML = `
-        <h3>Suggested Films</h3>
+    const matchedFilms = films.filter(film => 
+        userText.includes(film.mood)
+    );
+
+    if (matchedFilms.length === 0) {
+        resultsDiv.innerHTML = "<p>No exact match found. Try different words.</p>";
+        return;
+    }
+
+    resultsDiv.innerHTML = `
+        <h3>Suggested Films:</h3>
         <ul>
-            <li>In the mood for love</li>
-            <li>Eternal Sunshine of the Spotless Mind</li>
-            <li>Paris, Texas</li>
+            ${matchedFilms.map(film => `<li>${film.title}</li>`).join("")}
         </ul>
-    `
+    `;
 });
