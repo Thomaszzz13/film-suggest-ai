31f7935a2434f499ba1aaa6036fca72425da1639
Thomaszzz13 <thomaselnahas@gmail.com>
2026-03-02 23:45:42 +0200

    Restructure project to public folder and add branding assets

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, "public")));
 
 const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
